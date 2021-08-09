b67c93d822d9eeeda6575d9f3bbc4bbef6819d81
hirotoshimizu <hshimizu49@yahoo.co.jp>
2021-08-09 11:44:02 +0900

    update main

--- core/restconf/main.py
+++ core/restconf/main.py
@@ -8,9 +8,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     # disable Browsable API
-    # 'DEFAULT_RENDERER_CLASSES': [
-    #     'rest_framework.renderers.JSONRenderer',
-    # ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
